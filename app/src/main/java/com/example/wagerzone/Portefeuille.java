a04f6bd2f915d5c1828f0aabade980cc9ab17446
Jean-LoupIsReal <bbla9394@gmail.com>
2024-05-14 19:43:08 -0400

    bouton temporaire

--- app/src/main/java/com/example/wagerzone/Portefeuille.java
+++ app/src/main/java/com/example/wagerzone/Portefeuille.java
@@ -1,9 +1,6 @@
 package com.example.wagerzone;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,32 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class Portefeuille extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_portefeuille);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-            //TEST PUSH ANTHO
-            //Test Richard
-            //Test push max
-            //Test push JL
         });
-        Button btnPortefeuille = findViewById(R.id.btnPortefeuilleTemp);
-        btnPortefeuille.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        if(v.getId()==R.id.btnPortefeuilleTemp){
-            Intent intent = new Intent(MainActivity.this, Portefeuille.class);
-            startActivity(intent);
-            finish();
-        }
     }
 }
