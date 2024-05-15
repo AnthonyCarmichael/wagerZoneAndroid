e1e5cfcb1ab39c5aae15a1960ef1d3116d6f660d
Jean-LoupIsReal <bbla9394@gmail.com>
2024-05-14 22:18:48 -0400

    toutes pages incluses

--- app/src/main/java/com/example/wagerzone/Paiement.java
+++ app/src/main/java/com/example/wagerzone/Paiement.java
@@ -1,6 +1,5 @@
 package com.example.wagerzone;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,26 +10,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Portefeuille extends AppCompatActivity implements View.OnClickListener{
+public class Paiement extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_portefeuille);
+        setContentView(R.layout.activity_paiement);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button btnAjouteFonds = findViewById(R.id.buttonAjoutFonds);
-        btnAjouteFonds.setOnClickListener(this);
-    }
-    @Override
-    public void onClick(View v) {
-        if(v.getId()==R.id.buttonAjoutFonds){
-            Intent intent = new Intent(Portefeuille.this, Paiement.class);
-            startActivity(intent);
-        }
+        Button btnRetour = findViewById(R.id.btnRetour);
+        btnRetour.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                finish();
+            }
+        });
     }
 }
