67e3e193a442f669ae677319f2bfffecc2cfb7b2
Jean-LoupIsReal <131626426+Jean-LoupIsReal@users.noreply.github.com>
2024-05-16 10:22:56 -0400

    modele jl
    equipe, partie, sport

---
+++ app/src/main/java/Equipe.java
@@ -0,0 +1,57 @@
+public class Equipe {
+    private int _id_equipe;
+    private String _nom_equipe;
+    private String _entraineur;
+    private String _stade;
+    private int _match_joue;
+    private int _victoire;
+    private int _defaite;
+    private int _match_nul;
+    private int _defaite_prolongation;
+    private int _id_sport;
+    private int _id_ville;
+
+    public int get_id_equipe() {
+        return _id_equipe;
+    }
+
+    public String get_nom_equipe() {
+        return _nom_equipe;
+    }
+
+    public String get_entraineur() {
+        return _entraineur;
+    }
+
+    public String get_stade() {
+        return _stade;
+    }
+
+    public int get_match_joue() {
+        return _match_joue;
+    }
+
+    public int get_victoire() {
+        return _victoire;
+    }
+
+    public int get_defaite() {
+        return _defaite;
+    }
+
+    public int get_match_nul() {
+        return _match_nul;
+    }
+
+    public int get_defaite_prolongation() {
+        return _defaite_prolongation;
+    }
+
+    public int get_id_sport() {
+        return _id_sport;
+    }
+
+    public int get_id_ville() {
+        return _id_ville;
+    }
+}
