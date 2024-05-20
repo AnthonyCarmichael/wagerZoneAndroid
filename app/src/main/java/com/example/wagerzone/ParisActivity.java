eecc1b24fa9a24078d50ab038ffbcafb38b65df9
Maxime Malette <132301458+madmax9845@users.noreply.github.com>
2024-05-19 20:39:08 -0400

    Correction bouton retour page paris

--- app/src/main/java/com/example/wagerzone/ParisActivity.java
+++ app/src/main/java/com/example/wagerzone/ParisActivity.java
@@ -2,6 +2,8 @@ package com.example.wagerzone;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,6 +33,13 @@ public class ParisActivity extends AppCompatActivity implements RecyclerViewInte
         MyAdapter myAdapter = new MyAdapter(this, paris, this);
         recyclerView.setAdapter(myAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        Button button = (Button) findViewById(R.id.retour);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
     }
 
