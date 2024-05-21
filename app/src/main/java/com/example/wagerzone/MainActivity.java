89627d860c6b01c5188c1e3ad8d105ca945819d3
Jean-LoupIsReal <131626426+Jean-LoupIsReal@users.noreply.github.com>
2024-05-21 14:44:02 -0400

    fixMerge

--- app/src/main/java/com/example/wagerzone/MainActivity.java
+++ app/src/main/java/com/example/wagerzone/MainActivity.java
@@ -12,7 +12,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
     private Nav _nav;
 
