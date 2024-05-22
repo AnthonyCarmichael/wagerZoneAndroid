9e72eb3dd547418865373e4cf70e66cd25c9906d
Anthony Carmichael <anthonycarmichael0@gmail.com>
2024-05-22 12:36:54 -0400

    Spinner ville et pays fonctionnel

--- app/src/main/java/com/example/wagerzone/InscriptionActivity.java
+++ app/src/main/java/com/example/wagerzone/InscriptionActivity.java
@@ -1,8 +1,12 @@
 package com.example.wagerzone;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,9 +15,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InscriptionActivity extends AppCompatActivity {
 
     private Nav _nav;
+    private List<Pays> _pays;
+    private List<Ville> _villes;
+    private ArrayList<String> _nomVilles;
+    private ArrayList<String> _nomPays;
+    private Spinner _spinnerVille;
+    private Spinner _spinnerPays;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,5 +47,74 @@ public class InscriptionActivity extends AppCompatActivity {
         titre.setText(R.string.inscription);
         ImageButton userIcone = findViewById(R.id.userIcone);
         userIcone.setBackgroundResource(R.drawable.rounded_red);
+
+        // Set les spinner
+        set_spinnerPays();
+        set_spinnerVille();
     }
+
+    private void fetchVille(int idPays) {
+        SQLiteManager sqLiteManager = new SQLiteManager(InscriptionActivity.this);
+        _villes = sqLiteManager.fetchAllVillesByPaysId(idPays);
+    }
+
+    private void fetchPays() {
+        SQLiteManager sqLiteManager = new SQLiteManager(InscriptionActivity.this);
+        _pays = sqLiteManager.fetchAllPays();
+    }
+
+
+    private void set_spinnerVille(){
+        // Set les spinner:
+        fetchVille(1);
+        _nomVilles = new ArrayList<>();
+        for (Ville ville: _villes) {
+            _nomVilles.add(ville.get_nom_ville());
+        }
+        ArrayAdapter<String> arrayAdapterVilles=new ArrayAdapter<String>(InscriptionActivity.this,
+                android.R.layout.simple_spinner_dropdown_item, _nomVilles);
+
+        Spinner spinnerVille= findViewById(R.id.villes);
+
+        spinnerVille.setAdapter(arrayAdapterVilles);
+        this._spinnerVille = spinnerVille;
+        _spinnerPays.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                _villes.clear();
+                _nomVilles.clear();
+                fetchVille(_pays.get(position).get_id_pays());
+                for (Ville ville: _villes) {
+                    _nomVilles.add(ville.get_nom_ville());
+                }
+                ArrayAdapter<String> arrayAdapterVilles=new ArrayAdapter<String>(InscriptionActivity.this,
+                        android.R.layout.simple_spinner_dropdown_item, _nomVilles);
+
+                Spinner spinnerVille= findViewById(R.id.villes);
+
+                spinnerVille.setAdapter(arrayAdapterVilles);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+            }
+        });
+    }
+
+    private void set_spinnerPays(){
+        // Set les spinner:
+        fetchPays();
+        _nomPays= new ArrayList<>();
+        for (Pays pays: _pays) {
+            _nomPays.add(pays.get_nom_pays());
+        }
+        ArrayAdapter<String> arrayAdapterVilles=new ArrayAdapter<String>(InscriptionActivity.this,
+                android.R.layout.simple_spinner_dropdown_item, _nomPays);
+
+        Spinner spinnerPays= findViewById(R.id.pays);
+
+        spinnerPays.setAdapter(arrayAdapterVilles);
+        this._spinnerPays = spinnerPays;
+    }
+
 }
