73becce65ffe7ac78b73adb517f94ee83c8b1555
Anthony Carmichael <anthonycarmichael0@gmail.com>
2024-05-23 00:10:41 -0400

    Utilisation du gps pour setter automatiquement la ville et le pays

--- app/src/main/java/com/example/wagerzone/InscriptionActivity.java
+++ app/src/main/java/com/example/wagerzone/InscriptionActivity.java
@@ -1,6 +1,12 @@
 package com.example.wagerzone;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,13 +16,17 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class InscriptionActivity extends AppCompatActivity {
 
@@ -27,7 +37,11 @@ public class InscriptionActivity extends AppCompatActivity {
     private ArrayList<String> _nomPays;
     private Spinner _spinnerVille;
     private Spinner _spinnerPays;
-    
+    private String _gpsVille;
+    private String _gpsPays;
+
+    private Boolean _permissionGPS;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +52,6 @@ public class InscriptionActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
         // Set le nav et récupere le titre
         this._nav = new Nav(this,findViewById(android.R.id.content),InscriptionActivity.this);
         TextView titre = findViewById(R.id.titre);
@@ -51,6 +64,11 @@ public class InscriptionActivity extends AppCompatActivity {
         // Set les spinner
         set_spinnerPays();
         set_spinnerVille();
+        // Permission geolocalisation.
+        // Le callback permet d'attendre que l'utilisateur permet ou non le gps pour loader ensuite la page
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+
+
     }
 
     private void fetchVille(int idPays) {
@@ -62,7 +80,6 @@ public class InscriptionActivity extends AppCompatActivity {
         SQLiteManager sqLiteManager = new SQLiteManager(InscriptionActivity.this);
         _pays = sqLiteManager.fetchAllPays();
     }
-
 
     private void set_spinnerVille(){
         // Set les spinner:
@@ -89,10 +106,13 @@ public class InscriptionActivity extends AppCompatActivity {
                 }
                 ArrayAdapter<String> arrayAdapterVilles=new ArrayAdapter<String>(InscriptionActivity.this,
                         android.R.layout.simple_spinner_dropdown_item, _nomVilles);
+                _spinnerVille.setAdapter(arrayAdapterVilles);
+                if (_gpsVille!= null && _gpsPays.equals(_pays.get(position).get_nom_pays())){
+                    _nomVilles.add(_gpsVille);
+                    arrayAdapterVilles.notifyDataSetChanged();
+                    setSelectedItem(_spinnerVille,_gpsVille);
+                }
 
-                Spinner spinnerVille= findViewById(R.id.villes);
-
-                spinnerVille.setAdapter(arrayAdapterVilles);
             }
 
             @Override
@@ -115,6 +135,90 @@ public class InscriptionActivity extends AppCompatActivity {
 
         spinnerPays.setAdapter(arrayAdapterVilles);
         this._spinnerPays = spinnerPays;
+    }
+
+    private void setVillePaysWithGPS() {
+        GPStracker gpsTracker = new GPStracker(this);
+        Location location = gpsTracker.getLocation();
+        Geocoder geocoder = new Geocoder(this, Locale.FRENCH);
+        try {
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            if (addresses != null && !addresses.isEmpty()) {
+                Address address = addresses.get(0);
+                String cityName = address.getLocality(); // Ville
+                String countryName = address.getCountryName(); // Pays
+
+                // Log the city and country
+                Log.d("AddressInfo", "Ville: " + cityName);
+                Log.d("AddressInfo", "Pays: " + countryName);
+
+                // Set les spinners!
+                int cpt =0;
+                boolean villeTrouve = false;
+                boolean paysTrouve = false;
+                _gpsPays=countryName;
+                for (Pays pays : _pays) {
+                    if (pays.get_nom_pays().equals(countryName)) {
+                        setSelectedItem(_spinnerPays, pays.get_nom_pays());
+                        paysTrouve =true;
+                        cpt =0;
+                        for (Ville ville : _villes) {
+                            if (ville.get_nom_ville().equals(cityName)) {
+                                setSelectedItem(_spinnerVille, ville.get_nom_ville());
+                                villeTrouve=true;
+                                break;
+                            }
+                            else
+                                cpt++;
+                        }
+                        break;
+                    }
+                    else
+                        cpt++;
+                }
+                if (!paysTrouve){
+                    _nomPays.add(countryName);
+                    ArrayAdapter<String> arrayAdapterPays=new ArrayAdapter<String>(InscriptionActivity.this,
+                            android.R.layout.simple_spinner_dropdown_item, _nomPays);
+                    _spinnerPays.setAdapter(arrayAdapterPays);
+                    setSelectedItem(_spinnerVille,countryName);
+                }
+                if (!villeTrouve){
+                    _gpsVille=cityName;
+                    _nomVilles.add(cityName);
+                    ArrayAdapter<String> arrayAdapterVilles=new ArrayAdapter<String>(InscriptionActivity.this,
+                            android.R.layout.simple_spinner_dropdown_item, _nomVilles);
+                    _spinnerVille.setAdapter(arrayAdapterVilles);
+                    setSelectedItem(_spinnerVille,cityName);
+
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("GeocoderError", "Unable to get address from location", e);
+        }
+    }
+
+    private void setSelectedItem(Spinner spinner, String value) {
+        ArrayAdapter<String> adapter = (ArrayAdapter<String>) spinner.getAdapter();
+        int position = adapter.getPosition(value);
+        if (position >= 0) {
+            spinner.setSelection(position);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 123) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                _permissionGPS = true;
+                setVillePaysWithGPS();
+            } else {
+                _permissionGPS = false;
+            }
+        }
     }
 
 }
