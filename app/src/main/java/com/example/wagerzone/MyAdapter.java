4b60557e6fc8ce2686ef8ca7d7adf4a4b8b68832
Maxime Malette <132301458+madmax9845@users.noreply.github.com>
2024-05-23 12:13:27 -0400

    Suppression d'un paris terminer

--- app/src/main/java/com/example/wagerzone/MyAdapter.java
+++ app/src/main/java/com/example/wagerzone/MyAdapter.java
@@ -1,14 +1,22 @@
 package com.example.wagerzone;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.provider.Telephony;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONException;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -41,7 +49,7 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         pari = paris.get(position);
         SQLiteManager sqLiteManager = new SQLiteManager(context);
         try {
-            partie = sqLiteManager.getPartie(pari.get_partie());
+            partie = sqLiteManager.getPartie(pari.get_id_partie());
             visiteur = sqLiteManager.getEquipeVisiteur(partie.get_id_partie());
             receveur = sqLiteManager.getEquipeReceveur(partie.get_id_partie());
         } catch (IOException e) {
@@ -64,10 +72,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView match;
         TextView mise;
+
+
         public MyViewHolder(View itemView){
             super(itemView);
             mise = (TextView) itemView.findViewById(R.id.mise);
             match = (TextView) itemView.findViewById(R.id.match);
+            Button annuler = (Button) itemView.findViewById(R.id.annuler);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -79,6 +90,39 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
                             recyclerViewInterface.onItemClick(pos);
                         }
                     }
+                }
+            });
+            annuler.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Paris pari = paris.get(getAdapterPosition());
+                    SQLiteManager sqLiteManager = new SQLiteManager(context);
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setTitle(R.string.app_name);
+                    builder.setMessage("Voulez vous vraiment annuler ce paris?" + pari.get_id_paris());
+                    builder.setIcon(R.drawable.ic_launcher_foreground);
+                    builder.setPositiveButton("Oui", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.dismiss();
+                            try {
+                                sqLiteManager.supprimerParis(pari.get_id_paris());
+                                Intent intent = new Intent(v.getContext(), ParisActivity.class);
+                                startActivity(v.getContext(), intent, null);
+                                ((ParisActivity)context).finish();
+
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+
+                        }
+                    });
+                    builder.setNegativeButton("Non", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.dismiss();
+                        }
+                    });
+                    AlertDialog alert = builder.create();
+                    alert.show();
                 }
             });
         }
